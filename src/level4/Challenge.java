1b9bd1acbb9a83442a7541ce47cd95bdd5dd9d9a
rishikhurana2 <league@WTS7.attlocal.net>
2017-05-20 11:55:22 -0700

    Roomba Simulation

--- src/level4/Challenge.java
+++ src/level4/Challenge.java
@@ -20,11 +20,11 @@ public class Challenge extends Head {
 	
 	public void initialize() {
 		driveDirect(1000, 1000);
-		sleep(500);
+		sleep(450);
 		driveDirect(0, 105);
 		sleep(2400);
 		driveDirect(1000, 1000);
-		sleep(3000);
+		sleep(2900);
 		driveDirect(105, 0);
 		sleep(2700);
 		driveDirect(1000, 1000);
@@ -38,13 +38,23 @@ public class Challenge extends Head {
 		driveDirect(1000, 1000);
 		sleep(450);
 		driveDirect(0, 105);
-		sleep(1000);
+		sleep(1200);
 		driveDirect(1000, 1000);
 		sleep(300);
-		
+	
 	}
 
 	public void loop() {
-
+		if (isBumpedLeft()) {
+			driveDirect(0, 0);
+			sleep(1000);
+			System.out.println("bumped");
+			driveDirect(-100, -100);
+			sleep(2000);
+			driveDirect(0,105);
+			sleep(500);
+			driveDirect(1000, 1000);
+			sleep(1000);
+		} 
 	}
 }
